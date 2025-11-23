b9a9353d5103fab5b0e4c66f45285664265602f6
Esma Yılmaz <eeeyilmaz@gmail.com>
2025-11-23 19:33:49 +0300

    js object part 1

---
+++ jsObjects.js
@@ -0,0 +1,33 @@
+let person = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+console.log("Dot notation : " + person.firstName); // Dot notation : John
+console.log("Bracket notation : " + person["firstName"]); // Bracket notation : John 
+
+
+// *** assigning new value to property ****
+person.firstName = "Jane";
+console.log(person["firstName"]); // Jane
+
+
+// ***** add a new property to object ****
+person.gender = "Female";
+console.log(person); // { firstName: 'Jane', lastName: 'Doe', gender: 'Female' }
+
+
+// ****** delete a property from object ****
+delete person.gender;
+console.log(person); // { firstName: 'Jane', lastName: 'Doe' }
+
+// *** check any propert in object *****
+let isExist = "gender" in person;
+console.log("Is Exist : " + isExist); // Is Exist : false
+
+// **** reach each key in foreach loop (key keyword) *****
+
+for(let key in person)
+{
+    console.log(person[key]); // Jane \nDoe
+}
